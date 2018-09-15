a668c232c2ed6cc1d709b4cb3a54351d6eabe4b2
jin <winterchocolatte@gmail.com>
2018-09-15 04:06:06 -0400

    Scraped class data for garages.

--- lib/IWebScrape.dart
+++ lib/IWebScrape.dart
@@ -2,5 +2,5 @@ import 'package:ucf_parking/Garage.dart';
 import 'dart:async';
 
 abstract class IWebScrape {
-  Future<List<Garage>> scrape(String source);
+  static Future<List<Garage>> scrape(String source){}
 }
--- lib/WebScraper.dart
+++ lib/WebScraper.dart
@@ -1,11 +1,23 @@
 import 'package:ucf_parking/Garage.dart';
 import 'package:ucf_parking/IWebScrape.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' as parser;
+import 'package:html/dom.dart';
+import 'dart:async';
 
 class WebScraper implements IWebScrape {
 
   @override
-  List<Garage> scrape(String source) {
-    // TODO: work
-    return List();
+  static Future<List<Garage>> scrape (String source) async {
+    // TODO: Parse through parsed data.
+    http.Response response = await http.get(source);
+
+    Document document = parser.parse(response.body);
+
+    document.getElementsByClassName('dxgv').forEach((Element element){
+      print(element.text);
+    });
+
+    return List<Garage>();
   }
 }
